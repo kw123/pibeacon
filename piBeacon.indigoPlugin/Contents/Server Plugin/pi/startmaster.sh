f7febab758380e0d6fc7c0e260330ed9bc7a3d11
kw123 <karlwachs@me.com>
2020-11-16 21:29:55 -0600

    various changes
    0. fixed format for humidity UI values was {:0f} should be {:.0f}
    1. added optiuon to add new beacon mac # in menu, (accept new becons
       if you know a beacons mac numebr and you like to add it put the mac number there
       and the system will allow that mac numebr to be added as a beacon
    2. updated BLE-Analysis - detect new beacon tool
       the print outin plugin.log is now done is a nice format
       this can be used to identify beacons and understand the message structure
    3. for Minew TH + accelerator sensor (s1-Plus) added states amd options
    4. Autodetect battery level in BLE data and setup beacon accordingly for TLM and msg battery levels
       set supportBatterylevel = true and set method prop
    5. Added nuki door lock and UDM-pro and Flower care /mate

--- piBeacon.indigoPlugin/Contents/Server Plugin/pi/startmaster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# start master.py if not running , called from xwindows 
-sleep 2
-while [ 1 ]
-do
- ps -ef | grep 'master.py' | grep -v grep || nohup sudo /usr/bin/python /home/pi/pibeacon/master.py
- sleep 20
-done
